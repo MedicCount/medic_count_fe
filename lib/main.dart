7c91f994b2e162e6f5d7cd6590ab97ce2a4c44ff
xeff09 <wongchaita03@gmail.com>
2024-03-13 21:20:47 +0700

    fix: web user routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medic_count_fe/firebase_options.dart';
+import 'package:medic_count_fe/pages/notFoundPage.dart';
 import 'package:medic_count_fe/pages/homePage.dart';
-import 'package:medic_count_fe/pages/loginPage.dart';
 import 'package:medic_count_fe/pages/prePage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:medic_count_fe/pages/settingPage.dart';
-import 'package:medic_count_fe/pages/statPage.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  WidgetsFlutterBinding.ensureInitialized();
-  initializeFirebase();
+  await WidgetsFlutterBinding.ensureInitialized();
+  await initializeFirebase();
   runApp(const MyApp());
 }
 
@@ -22,32 +21,90 @@ Future<void> initializeFirebase() async {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  late Stream<User?> _authStateStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _authStateStream = FirebaseAuth.instance.authStateChanges();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var routes = {
-      '/': (context) => const HomePage(),
-      '/prePage': (context) => PrePage(),
-      '/loginPage': (context) => LoginPage(),
-      '/statPage': (context) => const StatPage(),
-      '/settingPage': (context) => const SettingPage(),
-    };
-
     return MaterialApp(
       title: 'Medic Count',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/prePage',
-      routes: routes,
+      home: AuthCheck(authStateStream: _authStateStream),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/home':
+            return _checkAuthAndRoute(const HomePage());
+          case '/pre':
+            return MaterialPageRoute(builder: (_) => const PrePage());
+          default:
+            return MaterialPageRoute(builder: (_) => const NotFoundPage());
+        }
+      },
     );
   }
 
+  MaterialPageRoute _checkAuthAndRoute(Widget page) {
+    return MaterialPageRoute(builder: (_) {
+      return StreamBuilder<User?>(
+        stream: _authStateStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            if (snapshot.hasData) {
+              return page;
+            } else {
+              return const PrePage();
+            }
+          }
+        },
+      );
+    });
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  final Stream<User?> authStateStream;
+
+  const AuthCheck({Key? key, required this.authStateStream}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: authStateStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const PrePage();
+          }
+        }
+      },
+    );
+  }
 }
