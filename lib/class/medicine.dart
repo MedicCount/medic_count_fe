c88b6b85842dbb9cd94d1544180e35d55c58a540
WeeranutCK <weeranut.c@ku.th>
2024-03-17 20:17:45 +0700

    Add medicine related class

---
+++ lib/class/medicine.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Medicine {
+  String _id;
+  String _name;
+  Image _image;
+  late int _counts;
+
+  Medicine(this._id, this._name, this._image);
+
+  void increaseCount(int amount) {
+    _counts += amount;
+  }
+
+  void decreaseCount(int amount) {
+    _counts -= amount;
+  }
+  
+  set setName(String name) {
+    _name = name;
+  }
+
+  set setImage(Image image) {
+    _image = image;
+  }
+
+  set setCount(int counts) {
+    _counts = counts;
+  }
+
+  String get getId => _id;
+  String get getName => _name;
+  Image get getImage => _image;
+  int get getCount => _counts;
+}
---
+++ lib/class/medicine_group.dart
@@ -0,0 +1,27 @@
+import 'medicine.dart';
+
+class MedicineGroup {
+  final List<Medicine> _medicineGroup;
+  late DateTime _timestamp;
+  
+  MedicineGroup(this._medicineGroup) {
+    _timestamp = DateTime.now();
+  }
+  
+  MedicineGroup.withTimestamp(this._medicineGroup, this._timestamp);
+
+  void addMedicine(Medicine medicine) {
+    _medicineGroup.add(medicine);
+  }
+
+  void removeMedicine(Medicine medicine) {
+    _medicineGroup.removeWhere((element) => element == medicine);
+  }
+
+  set setTimeStamp(DateTime timestamp) {
+    _timestamp = timestamp;
+  }
+
+  List<Medicine> get getMedicineGroup => _medicineGroup;
+  DateTime get getTimestamp => _timestamp;
+}
